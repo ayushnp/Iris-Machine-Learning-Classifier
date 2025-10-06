ac7c4f035d970ade9155f27a41ad0c7bdbaeb883
AYUSH N P <150500888+ayushnp@users.noreply.github.com>
2025-10-06 12:55:20 +0530

    Update data source URL for Iris dataset

--- Iris.py
+++ Iris.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 from sklearn.ensemble import RandomForestClassifier
 
-data=pd.read_csv(r"https://github.com/ayushnp/Iris-Machine-Learning-Classifier/blob/main/Iris-train.csv")
+data=pd.read_csv("https://raw.githubusercontent.com/ayushnp/Iris-Machine-Learning-Classifier/refs/heads/main/Iris-train.csv")
 print(data.head())
 
 x = data.iloc[:,1:-1]
